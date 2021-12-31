f027cd593c905c4d2f7bb8367fd6f7195a8e1b4e
Eisuke Suzuki <e.i.s.u.k.e1106@gmail.com>
2021-12-31 22:43:02 +0900

    エラー処理を修正

--- getTSfromURL.js
+++ getTSfromURL.js
@@ -1,22 +1,22 @@
-javascript:
 var resolution = '720p,best';
 var saveDir = "~/Documents/.ero";
+var newLine = '\n';
+
 var videoTags = document.getElementsByTagName("video");
 if (videoTags.length == 0){
     alert("can not find video tags");
-}
-var videoSrc = videoTags[0].src;
-var path = location.pathname;
-path = path.replace("/", "");
-path = path.replace("/", "");
-var date = new Date();
-var saveTime = "" + date.getFullYear() + (date.getMonth() + 1) + date.getDate() + date.getHours() + date.getMinutes() + date.getSeconds();
-var savePath = saveDir + '/' + path + '_' + saveTime + '.ts';
-var newLine = '\n';
+}else{
+    var videoSrc = videoTags[0].src;
 
-var command = ""; 
-command += 'echo -ne "\\033]0;' + path + '\\007";' + newLine;
-command += "streamlink 'hls://" + videoSrc + "' " + resolution + " -o " + savePath + " -f;" + newLine;
-command += 'osascript ~/Documents/.tools/end.scpt ' + path + ';' + newLine;
-command += 'exit;';
-alert(command);
+    var title = location.pathname.replaceAll('/','');
+    var date = new Date();
+    var saveTime = "" + date.getFullYear() + (date.getMonth() + 1) + date.getDate() + date.getHours() + date.getMinutes() + date.getSeconds();
+    var savePath = saveDir + '/' + title + '_' + saveTime + '.ts';
+    
+    var command = ""; 
+    command += 'echo -ne "\\033]0;' + title + '\\007";' + newLine;
+    command += "streamlink 'hls://" + videoSrc + "' " + resolution + " -o " + savePath + " -f;" + newLine;
+    command += 'osascript ~/Documents/.tools/end.scpt ' + title + ';' + newLine;
+    command += 'exit;';
+    alert(command);    
+}
